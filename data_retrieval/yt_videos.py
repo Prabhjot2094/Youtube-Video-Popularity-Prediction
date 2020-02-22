06c79f191fc129b4425637f4c418eed327d40cb3
Prabhjot Singh <prabhjot.2094@gmail.com>
2020-02-22 02:03:56 -0800

    Failed requests retried with unused credentials, raw data stored

--- data_retrieval/yt_videos.py
+++ data_retrieval/yt_videos.py
@@ -7,6 +7,7 @@
 import os
 import json
 import pprint
+import csv
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -17,6 +18,14 @@ import googleapiclient.errors
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 youtube = None
 
+def get_credentials():
+    names = os.listdir('data_retrieval/client_secret/')
+    for name in names[::-1]:
+        if 'json' not in name:
+            continue
+        yield name
+credential_files = get_credentials()
+
 def main():
     global youtube
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -25,7 +34,13 @@ def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "data_retrieval/client_secret.json"
+
+    try:
+        client_secrets_file = 'data_retrieval/client_secret/'+next(credential_files)#"data_retrieval/client_secret_9.json"
+        print(f"Using {client_secrets_file}")
+    except Exception as e:
+        print("Quotas for all credentials exhausted")
+        raise(e)
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
@@ -34,22 +49,60 @@ def main():
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
+#Restart function if quota gets exhausted
+#TODO : Handle partial data
+def restart_on_quota_exhaustion(fxn):
+
+    def inner(*args, **kwargs):
+        try:
+            return fxn(*args, **kwargs)
+        except Exception as e:
+            print("Restart ",str(e), kwargs)
+            main()
+            return fxn(*args, **kwargs)
+
+    return inner
+
+@restart_on_quota_exhaustion
+def execute_yt_call(api, params):
+    print(api, params)
+    if api=='channels':
+        request = youtube.channels().list(**params)
+    elif api=='playlists':
+        request = youtube.playlists().list(**params)
+    elif api=='playlist_items':
+        request = youtube.playlistItems().list(**params)
+    elif api=='video':
+        request = youtube.videos().list(**params)
+    else:
+        raise NotImplementedError()
+
+    response = request.execute()
+    return response
+
 #Get playlist id
 def get_channel_data(channel_identifier):
     #First request assuming channel_identifier is channel name
-    request = youtube.channels().list(
-        part="snippet,contentDetails,statistics",
-        forUsername="justforlaughscomedy"
-    )
-    response = request.execute()
+    #request = youtube.channels().list(
+    response =   execute_yt_call("channels",
+                                {
+                                    'part':"snippet,contentDetails,statistics",
+                                    'forUsername':channel_identifier
+                                })
+    #)
+    #response = request.execute()
     
+    #print("Response = ", response)
     if not response['pageInfo']['totalResults']:
         #Second(final) request assuming channel_identifier is channel ID
-        request = youtube.channels().list(
-            part="snippet,contentDetails,statistics",
-            id=channel_identifier
-        )
-        response = request.execute()
+        #request = youtube.channels().list(
+        response = execute_yt_call("channels", 
+                                {
+                                    'part':"snippet,contentDetails,statistics",
+                                    'id':channel_identifier
+                                })
+        #)
+        #response = request.execute()
     
     #If channel_identifier neither ID nor name, raise an Exception
     assert response['pageInfo']['totalResults'], Exception('Empty result')
@@ -65,14 +118,22 @@ def get_channel_data(channel_identifier):
 
 #For a given channel get all playlist_name and corresponding ids
 def get_all_playlists(channel_id):
+    '''
     request = youtube.playlists().list(
-        part="snippet,contentDetails",
-        channelId="UCxPmXKxJcDAhuYVHeFFyGTA",
-        maxResults=25
+        part="snippet",
+        maxResults=50,
+        channelId=channel_id
     )
     response = request.execute()
+    '''
+    response = execute_yt_call("playlists", 
+            {
+                'part':"snippet",
+                'maxResults':50,
+                'channelId':channel_id
+                })
 
-    playlists = {playlist['snippet']['title']: playlist['id'] for playlist in response['items']}
+    playlists = {playlist['snippet']['title'].lower().replace(' ', ''): playlist['id'] for playlist in response['items']}
     return playlists
 
 #Get playlist data
@@ -87,30 +148,74 @@ def get_playlist_data(playlist_id):
     }
 
     '''
+    '''
     request = youtube.playlistItems().list(
-        part="snippet,contentDetails",
-        maxResults=25,
+        part="contentDetails",
+        maxResults=50,
         playlistId=playlist_id
     )
     response = request.execute()
+    '''
+    response = execute_yt_call("playlist_items", 
+            {
+                'part':"contentDetails",
+                'maxResults':50,
+                'playlistId':playlist_id
+                })
 
+    total_videos = response['pageInfo']['totalResults']
     video_ids = [{'video_id' : video['contentDetails']['videoId']} for video in response['items']]
+    
+    total = 50
+    while 'nextPageToken' in response:
+        pageToken = response['nextPageToken']
+        '''
+        request = youtube.playlistItems().list(
+            part="contentDetails",
+            maxResults=50,
+            pageToken=pageToken,
+            playlistId=playlist_id
+        )
+        response = request.execute()
+        '''
+        response = execute_yt_call("playlist_items", 
+                {
+                    'part':"contentDetails",
+                    'maxResults':50,
+                    'pageToken':pageToken,
+                    'playlistId':playlist_id
+                    })
+        video_ids.extend([{'video_id' : video['contentDetails']['videoId']} for video in response['items']])
+        total+=50
+    
+    #main()
     return video_ids
 
 #Get video data
 def get_video_data(video_id):
+    '''
     request = youtube.videos().list(
-        part="snippet,contentDetails,statistics",
+        part="snippet,statistics",
         id=video_id
     )
     response = request.execute()
-    
+    '''
+    response = execute_yt_call('video', 
+            {
+                'part':"snippet,statistics",
+                'id':video_id
+                })
+
     retval = response['items'][0]['statistics']
-    retval['video_id'] = video_id
     
+    videoReleaseDate = response['items'][0]['snippet']['publishedAt']
+
+    retval['id'] = video_id
+    retval['published_date'] = videoReleaseDate
+
     return retval
 
-def get_data():
+def get_data(channel_identifier, playlist_name):
     '''
     channel_identifier  : ID or name
     playlist_name       : Name or blank
@@ -126,8 +231,9 @@ def get_data():
     #To store all data for this channel. To be returned at the end.
     aggregate_channel_data = dict()
 
-    channel_identifier = 'ChickComedy'
-    playlist_name = ''
+    #channel_identifier = 'ChickComedy'
+    #playlist_name = ''
+    
     #Get channel details for channel id and uploads playlist ID.
     try:
         channel_data = get_channel_data(channel_identifier)
@@ -143,6 +249,8 @@ def get_data():
     #Using playlist_id, extract all video IDs
     playlist_id = uploads_playlist_id if not playlist_name else playlist_name
     if playlist_id != uploads_playlist_id:
+        #Standardize playlist name
+        playlist_name = playlist_name.lower().replace(' ','')
         all_playlists = get_all_playlists(channel_id)
         playlist_id = all_playlists[playlist_name]
 
@@ -161,7 +269,53 @@ def get_data():
     pp.pprint(aggregate_channel_data)
     return aggregate_channel_data
 
+def process_channel_list():
+    #Read from csv
+    f = open('/users/prabhjotsingh/Downloads/comedy_channels.csv', 'r')
+    csv_reader = csv.reader(f)
+
+    headers = next(csv_reader)
+    file_data = [{col:val for col,val in zip(headers, row)} for row in csv_reader]
+    
+    f.close()
+
+    #Aggregate data for all channels
+    final_data = list() 
+    for i, row in enumerate(file_data):
+        print(f"Current Item : {i}")
+        try:
+            channel_data = get_data(row['ID'], row['Playlist ID'])
+            if not channel_data:
+                continue
+            final_data.append(channel_data)
+        except Exception as e:
+            print(str(e))
+            break
+
+    #Write to csv
+    headers = ['title', 'published_at', 'subscriberCount', 'videoCount', 'viewCount', 'video_published_date', 'video_commentCount', 'video_viewCount', 'video_likeCount', 'video_dislikeCount', 'video_favoriteCount', 'video_id']
+    channel_stats_to_store = ['subscriberCount', 'videoCount', 'viewCount']
+    f = open('raw_data.csv', 'w')
+    csv_writer = csv.writer(f)
+
+    csv_writer.writerow(headers)
+    for channel in final_data:
+        row = dict()
+        row['published_at'] = channel['published_at']
+        row['title'] = channel['title']
+        for name in channel_stats_to_store:
+            row[name] = channel['channel_stats'][name]
+        
+        for video in channel['videos']:
+            for key, val in video.items():
+                row['video_'+key] = val
+            csv_writer.writerow([row[col] for col in headers])
+
+    f.close()
+
+    
 if __name__ == "__main__":
     print("Test")
     main()
-    get_data()
+    process_channel_list()
+    #get_data()
