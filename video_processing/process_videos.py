2222a083bbf25aac88c41259ea294232c76551ab
Prabhjot Singh <prabhjot.2094@gmail.com>
2020-03-25 20:51:47 -0700

    Multiprocessing enabled for OPenface and removed for video download

--- video_processing/process_videos.py
+++ video_processing/process_videos.py
@@ -7,7 +7,7 @@ import config
 import csv
 import cv2
 import shutil
-from multiprocessing import Pool, Manager, cpu_count
+from multiprocessing import Pool, Manager, cpu_count, Process, Queue
 
 REQUIRED_FPS = 3
 SKIP_FRAMES = 60//REQUIRED_FPS+1
@@ -49,21 +49,16 @@ def compress_video(video_location):
         frame+=1
     return output_dir 
 
-class Openface():
+def extract_features(video_location):
     '''
-    Extract facial features of a given video
+    Run openface on given video, read the extracted features, return the the extracted features
     '''
-    @classmethod
-    def extract_features(self, video_location):
-        '''
-        Run openface on given video, read the extracted features, return the the extracted features
-        '''
-        output_path = get_file_directory(video_location) + '/processed_video/' + get_file_name(video_location)
-        args = [config.OPENFACEPATH, "-fdir", video_location, "-out_dir", output_path]
-        
-        proc = subprocess.run(args)
-        #features = read_features(video_location)
-        return proc.returncode
+    output_path = get_file_directory(video_location) + '/processed_video/' + get_file_name(video_location)
+    args = [config.OPENFACEPATH, "-fdir", video_location, "-out_dir", output_path]
+    
+    proc = subprocess.run(args)
+    #features = read_features(video_location)
+    return proc.returncode
 
 class ProcessVideos():
     '''
@@ -97,24 +92,27 @@ def bitrate_helper(abr):
 
     return num
 
+'''
+-10 video/audio failed download
+-11 Compression failed
+'''
 class Video():
     '''
     Represents a single video and features associated with it
     '''
     def __init__(self, video_id):
+        self.id = video_id
         self.video_location, self.audio_location = self.get_yt_video_data(video_id)
         if not self.video_location and not self.audio_location:
             self.features_extracted_status = -10
         else:
-            self.frames_location = compress_video(self.video_location)
-            self.features_extracted_status = Openface().extract_features(self.frames_location)
-            print(f"Video feature extraction for {video_id} returned with status code {self.features_extracted_status}")
+            try:
+                self.frames_location = compress_video(self.video_location)
+                self.features_extracted_status = 0
+            except:
+                self.features_extracted_status = -11#Openface().extract_features(self.frames_location)
+            #print(f"Video feature extraction for {video_id} returned with status code {self.features_extracted_status}")
     
-            #Delete the video files
-            os.remove(self.video_location)
-            os.remove(self.audio_location)
-            shutil.rmtree(self.frames_location)
-
 
     
     def get_yt_video_data(self, video_id, location=None):
@@ -153,62 +151,119 @@ class Video():
 def print_error(e):
     print(e)
 
-def process_videos_helper(video_ids):
-    video_proc_obj = ProcessVideos(video_ids)
+def extract_features_helper(video):
     #Store id for failed videos 
-    for status, video_id in zip(video_proc_obj.video_status, video_ids):
-        if status!=0:
-            errors_list.append([video_id, status, 'video'])
-        else:
-            processed_list.append(video_id)
-            #videos_failed_csv.writerow([video_id, status])
+    extracted_status = extract_features(video.frames_location)
+    #for status, video_id in zip(video_proc_obj.video_status, video_ids):
+    if extracted_status!=0:
+        errors_list.append([video.id, extracted_status, 'video'])
+    else:
+        #Delete the video files
+        os.remove(video.video_location)
+        os.remove(video.audio_location)
+        shutil.rmtree(video.frames_location)
+
+        processed_list.append(video.id)
     
     ###############################
     #Audio processing
     
     #Store id for failed audio files
-    for status, video_id in zip(video_proc_obj.audio_status, video_ids):
-        if status!=0:
-            errors_list.append([video_id, status, 'audio'])
-        else:
-            processed_list.append(video_id)
-            #audios_failed_csv.writerow([video_id, status])
+    #for status, video_id in zip(video_proc_obj.audio_status, video_ids):
+    #    if status!=0:
+    #        errors_list.append([video_id, status, 'audio'])
+    #    else:
+    #        processed_list.append(video_id)
+    #        #audios_failed_csv.writerow([video_id, status])
 
+def process_videos_helper():
+    while not video_ids.empty():
+        video_id = video_ids.get()
+        print("Helper", video_ids)
+        video = Video(video_id)
+        
+        #Store id for failed videos
+        status = video.features_extracted_status
+        print(f"Status for {video_id} = {status}")
+        if status!=0:
+            errors_list.append([video_id, status, 'video'])
+        else:
+            downloaded_que.put(video)
+
+            #videos_failed_csv.writerow([video_id, status])
+    
+        ###############################
+        #Audio processing
+        
+        #Store id for failed audio files
+        '''
+        for status, video_id in zip(video_proc_obj.audio_status, video_ids):
+            if status!=0:
+                errors_list.append([video_id, status, 'audio'])
+            else:
+                processed_list.append(video_id)
+                #audios_failed_csv.writerow([video_id, status])
+        '''
 
 def main():
+    #global video_ids
     #Read raw data
     f = open('processed_video_ids.json', 'r')
     processed_list.extend(json.load(f))
     f.close()
 
-    video_ids = list()
+    #video_ids = list()
     
     f = open('Data/raw_data.csv')
     c = csv.reader(f)
     #Get rid of the header row
     next(c)
+    x=0
     for i, row in enumerate(c):
-        if i%100==0: #create sets of 100 ids which'll be distributed to the spawned processes
-            video_ids.append(list())
         if row[12] in processed_list:
             continue
-        video_ids[-1].append(row[12])
+        video_ids.put(row[12])
+        x+=1
+        if x>=7:
+            break
     f.close()
     
     #Fetch details of all videos
     #video_ids = video_ids[:1]+['ewfherher']#, 'wO0a2C6PqtY']
     start = time.time()
     
+    #video_ids = video_ids[:5]
     #video_ids = [video_ids[3][:2]]
     #video_ids = [video_ids[0][:1], video_ids[0][1:]+[' ##################']]
 
-    NUM_WORKERS = cpu_count()*2
+    ###################################
+
+    download_video_proc = Process(target=process_videos_helper)
+    download_video_proc.start()
     
-    p = Pool(processes=NUM_WORKERS)
-    p.map_async(process_videos_helper, video_ids, error_callback=print_error)
-    p.close()
-    p.join()
-    
+    openface_processes = list()
+    MAX_PROCESSES = cpu_count()-1
+    while not video_ids.empty() or not downloaded_que.empty() or openface_processes:
+        print("downloading ", video_ids, video_ids.empty(), downloaded_que.empty())
+        time.sleep(2)
+        openface_processes = [p for p in openface_processes if p.is_alive()]
+        if not downloaded_que.empty():
+            if len(openface_processes)>MAX_PROCESSES:
+                print("MAX Procs running")
+                continue
+            print("Sarting new OpenFace proc")
+            #Get running procs
+            #If running procs<max, spawn, else continue
+            video = downloaded_que.get()
+            p = Process(target=extract_features_helper, args=(video,))
+            p.start()
+            openface_processes.append(p)
+            print("Vid ID", video.id)
+    #openface_procs = Process(target)
+
+    ###################################
+
+    print("downloading ", video_ids, video_ids.empty(), downloaded_que.empty())
     print("Processing time = ", time.time()-start)
     print("Saving processed video ids and Terminating program !!!")
     f = open('processed_video_ids.json', 'w')
@@ -222,6 +277,8 @@ def main():
     errors.close()
 
 #Objects shared between processes
+video_ids = Queue()
 errors_list = Manager().list()
 processed_list = Manager().list()
+downloaded_que = Queue()#Manager().Queue()
 main()
